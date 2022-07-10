06423907882d6755981aa2de58c60fbfa1323e68
luodyfdg <89191620+luodyfdg@users.noreply.github.com>
2022-07-10 14:47:55 +0800

    Add files via upload

---
+++ adbtools.py
@@ -0,0 +1,67 @@
+from os import system
+
+a = 1
+while a <= 1:
+    print("测试版可能有bug，请在support.qq.com/products/410627中反馈\n")
+    print("""【0】退出【1】查看设备连接情况【2】重启至rec
+【3】重启至fastboot【4】查看fastboot连接情况【5】刷入rec分区【6】刷入boot分区
+【7】安装应用""")
+    print()
+    skna = input("请输入数字：")
+    if(skna == '0'): 
+        exit() 
+    if(skna == '1'):
+        system("cls")
+        system("adb devices")
+        system("pause")
+    if(skna == '2'):
+        system("cls")
+        print("重启中...")
+        system("adb reboot recovery")
+        print("已重启，请确认")
+        system("pause")
+    if(skna == '3'):
+        system("cls")
+        print("重启中...")
+        system("adb reboot fastboot")
+        print("已重启，请确认")
+        system("pause")
+    if(skna == '4'):
+        system("cls")
+        system("fastboot devices")
+        system("pause")
+    if(skna == '5'):
+        system("cls")
+        print("刷入rec分区有风险，请三思而后行\n")
+        system("pause")
+        system("cls")
+        print("请进入fastboot模式后继续\n")
+        system("pause")
+        reciN = input("请将rec镜像放置于主目录，并输入文件名：")
+        astadb = f"fastboot flash recovery {reciN}"
+        system(astadb)
+        system("pause")
+    if(skna == '6'):
+        system("cls")
+        print("刷入boot分区有风险，请确认")
+        system("pause")
+        system("cls")
+        print("请进入fastboot模式后继续\n")
+        system("pause")
+        bootN = input("请将boot镜像放置于主目录，并输入文件名: ")
+        booadb = f"fastboot flash boot {bootN}"
+        system(booadb)
+        print("已刷入，请确认")
+        system("pause")
+    if(skna == '7'):
+        system("cls")
+        print("请在开发者选项中打开“USB安装应用”再继续！\n")
+        system("pause")
+        print()
+        apkN = input("请把apk放置于程序主目录，并输入apk文件名: ")
+        apkS = f"adb install {apkN}"
+        print("如果手机提示是否安装，请确认")
+        system(apkS)
+        print("已安装，请确认")
+        system("pause")
+    system("cls")
